5d692555f1563a627810e3b747267d79de1f10ff
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-13 18:06:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toUpperCase === string){
+  if (string === string.toUpperCase){
     return "YES INDEED!"
   }
-  else if (string.toLowerCase === string){
+  else if (string === string.toLowerCase){
     return "I can't hear you!"
   }
   else if (string === "I love you, Grandma.") {
