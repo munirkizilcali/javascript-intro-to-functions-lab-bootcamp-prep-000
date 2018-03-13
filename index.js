e8317ef992c4889a23296f20b7a4dd256e23e771
Munir Kizilcali <munir.kizilcali@gmail.com>
2018-03-13 17:59:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGranma(string) {
+function sayHiToGrandma(string) {
   if (string.toUpperCase === string){
     return "YES INDIID!"
   }
